90f73a1a5579dd104bfae99cd737feb5cb77e2ad
RedAsteroid <123588653+RedAsteroid@users.noreply.github.com>
2025-06-15 05:21:23 +0800

    移除 DR 卡传送脱困逻辑，统一 FateMacro 默认名称

--- Multi Zone Farming CN.lua
+++ Multi Zone Farming CN.lua
@@ -5,9 +5,9 @@
 *                                Version 1.0.1                                 *
 ********************************************************************************
 
-多地图 FATE Farming 脚本，设计用于配合 Fate Farming.lua 使用。
+多地图 FATE Farming 脚本，需要搭配 Fate Farming.lua 使用。
 此脚本会按顺序遍历地图列表，在每个地图进行 FATE 伐木，直到地图中没有符合条件的FATE，
-然后传送至下一区域并重新启动 FATE Farming 脚本。 
+然后传送至下一地图并重新启动 FATE Farming 脚本。 
 
 作者: pot0to (https://ko-fi.com/pot0to)
         
@@ -22,7 +22,7 @@
 ********************************************************************************
 ]]
 
-FateMacro = "Fate Farming Companion"      -- Fate Farming 脚本在 Snd 插件中的自定义名称
+FateMacro = "Fate Farming"      -- Fate Farming 脚本在 Snd 插件中的自定义名称
 
 -- 在 Fate Farming 脚本中按 Ctrl+F 查找 zoneId，或使用 Godbert 工具查询
 ZonesToFarm =
@@ -47,41 +47,14 @@ ZonesToFarm =
 **************************************************************
 ]]
 
-StopScript = false
 CharacterCondition = {
     casting=27,
     betweenAreas=45
 }
 
-function EscapeTeleportStuckDR()
-    --检查 Daily Routines 插件是否启用，如果没有启用则停止循环，有则自动进出本
-    if not HasPlugin("DailyRoutines") then
-        LogInfo("[FATE] 准备处理传送卡死，但由于没有安装或启用 Daily Routines 插件此功能无法生效，脚本即将停止，感谢您的使用，祝您好运！")
-        yield("/e [FATE] 准备处理传送卡死，但由于没有安装或启用 Daily Routines 插件此功能无法生效，脚本即将停止，感谢您的使用，祝您好运！")
-        StopScript = true
-    else
-        LogInfo("[FATE] 准备处理传送卡死！")
-        yield("/e [FATE] 准备处理传送卡死！")
-
-        --功能启用初始化
-        yield("/pdr load AutoCommenceDuty")
-        yield("/pdr load AutoJoinExitDuty")
-
-        yield("/wait 1") --1秒延迟避免异常发生
-
-        -- 执行进出副本
-        yield("/pdr joinexitduty")
-    end
-end
-
 function TeleportTo(aetheryteName)
     yield("/tp "..aetheryteName)
     yield("/wait 1") -- wait for casting to begin
-
-    if IsAddonVisible("_TextError") and GetNodeText("_TextError", 1) == "无法发动传送，其他传送正在进行。" then --使用 Daily Routines 插件处理卡顿，如果没安装则停用脚本
-        EscapeTeleportStuckDR()
-    end
-
     while GetCharacterCondition(CharacterCondition.casting) do
         yield("/wait 1")
     end
@@ -95,18 +68,18 @@ end
 yield("/at y")
 FarmingZoneIndex = 1
 OldBicolorGemCount = GetItemCount(26807)
-while StopScript == false do
+while true do
     if not IsPlayerOccupied() and not IsMacroRunningOrQueued(FateMacro) then
         if GetCharacterCondition(2) or GetCharacterCondition(26) or GetZoneID() == ZonesToFarm[FarmingZoneIndex].zoneId then
             LogInfo("[MultiZone] Starting FateMacro")
             yield("/snd run "..FateMacro)
             repeat
-                yield("/wait 1") --等待太久了，缩短时间
+                yield("/wait 1") --减少等待时间
             until not IsMacroRunningOrQueued(FateMacro)
             LogInfo("[MultiZone] FateMacro has stopped")
             NewBicolorGemCount = GetItemCount(26807)
             if NewBicolorGemCount == OldBicolorGemCount then
-                yield("/echo 双色宝石: "..NewBicolorGemCount)
+                yield("/echo Bicolor Count: "..NewBicolorGemCount)
                 FarmingZoneIndex  = (FarmingZoneIndex % #ZonesToFarm) + 1
             else
                 OldBicolorGemCount = NewBicolorGemCount
